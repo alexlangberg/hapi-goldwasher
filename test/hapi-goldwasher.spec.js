1425b86ffbe6c2a259f1a0576870d56c509397ec
Alex Langberg <alexlangberg@gmail.com>
2015-05-14 23:12:28 +0200

    Route set up.

--- test/hapi-goldwasher.spec.js
+++ test/hapi-goldwasher.spec.js
@@ -4,48 +4,51 @@ var chai = require('chai');
 chai.use(require('chai-things'));
 var should = chai.should();
 var Hapi = require('hapi');
+var defaultPath = '/goldwasher';
 
-describe('goldwasher plugin', function () {
+describe('goldwasher plugin with default options', function() {
   var server = new Hapi.Server();
-  server.connection({port:80});
+  server.connection({port: 7979});
 
-  it('loads', function (done) {
+  it('loads', function(done) {
     server.register({
       register: require('../lib/hapi-goldwasher.js')
-    }, function(err) {
-      should.not.exist(err);
+    }, function(error) {
+      should.not.exist(error);
       done();
     });
   });
 
-  //it('registers routes', function (done) {
-  //  var table = server.table();
-  //  table.should.have.length(1);
-  //  table[0].table[0].path.should.equal('/');
-  //  done();
-  //});
-  //
-  //it('has a working route', function (done) {
-  //  server.settings.app = {
-  //    config: {
-  //      product: {
-  //        info: {}
-  //      }
-  //    }
-  //  };
-  //  server.inject({method: 'GET', url:'/'}, function(response) {
-  //    response.statusCode.should.equal(200);
-  //    done();
-  //  });
-  //});
-  //
-  //it('has a working route even without info object', function (done) {
-  //  server.settings.app.config = {
-  //    product: {}
-  //  };
-  //  server.inject({method: 'GET', url:'/'}, function(response) {
-  //    response.statusCode.should.equal(200);
-  //    done();
-  //  });
-  //});
+  it('registers default route', function(done) {
+    var table = server.table();
+    table.should.have.length(1);
+    table[0].table[0].path.should.equal(defaultPath);
+    done();
+  });
+
+  it('has a working route', function(done) {
+    server.inject({method: 'GET', url: defaultPath}, function(response) {
+      console.log(response.result);
+      response.statusCode.should.equal(200);
+      done();
+    });
+  });
+});
+
+describe('goldwasher throws on invalid options', function() {
+  var server = new Hapi.Server();
+  server.connection({port: 7980});
+
+  it('loads', function(done) {
+    server.register({
+      register: require('../lib/hapi-goldwasher.js'),
+      options: {path: 'dfg'}
+    }, function(error) {
+      console.log('dodndn');
+      console.log('erråå', error);
+
+      //error.should.be.an(Error);
+      done();
+    });
+  });
 });
