a64eee7becd9d6f7179ae29ef3e86f1be33c7289
Alex Langberg <alexlangberg@gmail.com>
2015-05-14 21:23:14 +0200

    First commit.

--- test/hapi-goldwasher.spec.js
+++ test/hapi-goldwasher.spec.js
@@ -3,13 +3,49 @@
 var chai = require('chai');
 chai.use(require('chai-things'));
 var should = chai.should();
-var validator = require('validator');
-var cheerio = require('cheerio');
-var plugin = require('../lib/hapi-goldwasher');
-var R = require('ramda');
+var Hapi = require('hapi');
 
-//describe('init', function() {
-//  it('loads', function(done) {
-//    done();
-//  });
-//});
+describe('goldwasher plugin', function () {
+  var server = new Hapi.Server();
+  server.connection({port:80});
+
+  it('loads', function (done) {
+    server.register({
+      register: require('../lib/hapi-goldwasher.js')
+    }, function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  //it('registers routes', function (done) {
+  //  var table = server.table();
+  //  table.should.have.length(1);
+  //  table[0].table[0].path.should.equal('/');
+  //  done();
+  //});
+  //
+  //it('has a working route', function (done) {
+  //  server.settings.app = {
+  //    config: {
+  //      product: {
+  //        info: {}
+  //      }
+  //    }
+  //  };
+  //  server.inject({method: 'GET', url:'/'}, function(response) {
+  //    response.statusCode.should.equal(200);
+  //    done();
+  //  });
+  //});
+  //
+  //it('has a working route even without info object', function (done) {
+  //  server.settings.app.config = {
+  //    product: {}
+  //  };
+  //  server.inject({method: 'GET', url:'/'}, function(response) {
+  //    response.statusCode.should.equal(200);
+  //    done();
+  //  });
+  //});
+});
