a5e2aeedf8bfc2cfd673612a432faaa320fc9c7b
Alex Langberg <alexlangberg@gmail.com>
2015-05-16 18:04:32 +0200

    Needle and goldwasher implemented.

--- test/hapi-goldwasher.spec.js
+++ test/hapi-goldwasher.spec.js
@@ -6,6 +6,21 @@ var should = chai.should();
 var Hapi = require('hapi');
 var defaultPath = '/goldwasher';
 
+var urlWithParameters = function(obj) {
+  var str = defaultPath + '?';
+  for (var key in obj) {
+    if (!obj.hasOwnProperty(key)) { continue; }
+
+    if (str !== defaultPath + '?') {
+      str += '&';
+    }
+
+    str += key + '=' + encodeURIComponent(obj[key]);
+  }
+
+  return str;
+};
+
 describe('setup and routes', function() {
   var server = new Hapi.Server();
   server.connection({port: 7979});
@@ -26,16 +41,42 @@ describe('setup and routes', function() {
     done();
   });
 
-  it('has a working route', function(done) {
+  it('responds with default message if no url parameter', function(done) {
     server.inject({method: 'GET', url: defaultPath}, function(response) {
       response.statusCode.should.equal(200);
+      response.result.should.have.property('name');
+      response.result.should.have.property('description');
+      response.result.should.have.property('docs');
+      response.result.should.have.property('uri');
+      response.result.should.have.property('parameters');
+      response.result.should.have.property('goldwasher');
       done();
     });
   });
 
   it('can request with url parameter', function(done) {
-    var url = defaultPath + '?url=' + encodeURIComponent('http://www.dr.dk');
+    var url = urlWithParameters({ url: 'http://www.dr.dk'});
+    //console.log(url);
     server.inject({method: 'GET', url: url}, function(response) {
+      response.statusCode.should.equal(200);
+      done();
+    });
+  });
+
+  it('can request with all other parameters', function(done) {
+    var url = urlWithParameters({
+      url: 'http://www.dr.dk',
+      selector: 'h1',
+      search: 'foo',
+      output: 'json',
+      limit: 5,
+      filterKeywords: 'bar; baz',
+      filterTexts: 'bar is good, baz is evil',
+      filterLocale: 'en'
+    });
+    //console.log(url);
+    server.inject({method: 'GET', url: url}, function(response) {
+      //console.log(response.result);
       response.statusCode.should.equal(200);
       done();
     });
@@ -44,7 +85,7 @@ describe('setup and routes', function() {
 
 describe('options validation', function() {
   var server = new Hapi.Server();
-  server.connection({port: 7980});
+  server.connection({port: 7979});
 
   it('throws on invalid path', function(done) {
     try {
